518531882e7707fdc032af69468858ca613b6da3
MbusyaNgesa <theengesa@gmail.com>
2024-07-30 15:18:50 +0300

    Adding Items to list

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-var form = document.getElementById('form');
-var itemList = document.getElementById('items');
+// var form = document.getElementById('form');
+// var itemList = document.getElementById('items');
 
-form.addEventListener('add', addItem);
+// form.addEventListener('add', addItem);
 
-function addItem(e){
-    e.preventDefault();
+// function addItem(e){
+//     e.preventDefault();
 
-    var newItem = document.getElementById('item').value;
+//     var newItem = document.getElementById('items').value;
 
-    var li = document.createElement('li');
-    li.className = 'listItem';
-    li.appendChild(document.createTextNode(newItem));
+//     var li = document.createElement('li');
+//     li.className = 'listItem';
+//     li.appendChild(document.createTextNode(newItem));
 
-    itemList.appendChild(li);
+//     itemList.appendChild(li);
+// }
+
+let addButton = document.querySelector('.add');
+let listItems = document.querySelector('#items');
+
+addButton.addEventListener('click', addTask);
+
+function addTask(e){
+    let newTask = document.querySelector('.input').value; //Get input value in the input field
+    // console.log(newTask);
+    let list = document.createElement('li');
+    // console.log(list);
+    list.className = 'listItem';
+    // console.log(list);
+    list.appendChild(document.createTextNode(newTask));
+    listItems.appendChild(list);
 }
