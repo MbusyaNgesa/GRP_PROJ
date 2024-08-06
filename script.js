d44b6c6972246db23c0dcb8045a328a072c8f20d
MbusyaNgesa <theengesa@gmail.com>
2024-08-06 18:08:42 +0300

    Deposit Event

--- script.js
+++ script.js
@@ -1,53 +1,68 @@
-let addButton = document.querySelector('.add');
-let listItems = document.querySelector('#items');
-let checkTask = document.querySelector('.checkBox');
+let addButton = document.querySelector(".add");
+let listItems = document.querySelector("#items");
+let checkTasks = document.querySelectorAll(".checkBox");
 
-addButton.addEventListener('click', addTask);
-listItems.addEventListener('click', removeTask);
-checkTask.addEventListener('click', checkBox);
+checkTasks.forEach((checkTask) => {
+  checkTask.addEventListener("click", checkBox);
+});
 
-function addTask(e){
-    //Adding a task to the input field then have it appear on the screen
-    let newTask = document.querySelector('.input').value; //Get input value in the input field
-    let list = document.createElement('li');
-    list.className = 'listItem';
-  
-    list.appendChild(document.createTextNode(newTask)); //The new added element is added to the screen
-    listItems.appendChild(list);
+addButton.addEventListener("click", addTask);
+listItems.addEventListener("click", removeTask);
 
-    //Creating the delete button and adds when "ADD" button is clicked
-    let deleteBtn = document.createElement('button');
-    // console.log(deleteBtn);
-    deleteBtn.className = 'btnDel delete';
-    // console.log(deleteBtn)
-    deleteBtn.appendChild(document.createTextNode('Delete')); //Adding button content
-    list.appendChild(deleteBtn) //Append button to list 
+function addTask(e) {
+  //Adding a task to the input field then have it appear on the screen
+  let newTask = document.querySelector(".input").value; //Get input value in the input field
+  let list = document.createElement("li");
+  list.className = "listItem flex items-center";
 
-    //Add Checkbox
-    let checkBtn = document.createElement('input');
-    // console.log(checkBtn);
-    checkBtn.type = 'checkbox';
-    checkBtn.className = 'ml-2';
-    // console.log(checkBtn);
-    checkBtn.appendChild(document.createTextNode(checkBtn));
-    list.appendChild(checkBtn); 
+  //Add Checkbox
+  let checkBtn = document.createElement("input");
+  // console.log(checkBtn);
+  checkBtn.type = "checkbox";
+  checkBtn.className = "mr-2";
+  // console.log(checkBtn);
+  //   checkBtn.appendChild(document.createTextNode(checkBtn));
+  checkBtn.addEventListener("click", checkBox);
+  list.appendChild(checkBtn);
+
+  let taskText = document.createElement("span");
+  taskText.appendChild(document.createTextNode(newTask));
+  list.appendChild(taskText); //The new added element is added to the screen
+
+  //Creating the delete button and adds when "ADD" button is clicked
+  let deleteBtn = document.createElement("button");
+  // console.log(deleteBtn);
+  deleteBtn.className = "btnDel delete flex items-center";
+  // console.log(deleteBtn)
+  let trash = document.createElement("img");
+
+  trash.src = "./public/img/trash.svg";
+  trash.alt = "Delete Icon";
+  trash.width = 40;
+
+  deleteBtn.appendChild(trash); //Adding button content
+  list.appendChild(deleteBtn); //Append button to list
+
+  listItems.appendChild(list);
 }
 
-function removeTask(e){
-    if(e.target.classList.contains('btnDel')){
-        let deleteTask = e.target.parentElement; //Targeting parentElement where 'btnDel' is at
-        console.log(deleteTask)
-        deleteTask.remove();
-    }
+function removeTask(e) {
+  console.log("here");
+  if (e.target.parentElement.classList.contains("btnDel")) {
+    let deleteTask = e.target.parentElement.parentElement.parentElement; //Targeting parentElement where 'btnDel' is at
+    console.log(deleteTask);
+    deleteTask.remove();
+  }
 }
 
-function checkBox(e){
-    let task = document.querySelector('.listItem')
-    
-    if(this.checked){
-        task.style.textDecoration = 'line-through'
-    }
-    else{
-        task.style.textDecoration = 'none'
-    }
+function checkBox(e) {
+  //   let task = document.querySelector(".listItem");
+
+  let taskText = e.target.nextElementSibling;
+
+  if (e.target.checked) {
+    taskText.style.textDecoration = "line-through";
+  } else {
+    taskText.style.textDecoration = "none";
+  }
 }
